37784b47539c2103662bc1edf645870c322eadce
ananay singh <ananaysingh0025@gmail.com>
2017-03-05 11:51:00 +0530

    Binary Tree Structure

---
+++ src/com/Tree/BinaryTree.java
@@ -0,0 +1,45 @@
+package com.Tree;
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+
+    public static Node newNode(int data){
+        Node n = new Node();
+        n.left = null;
+        n.right = null;
+        n.data = data;
+        return n;
+    }
+}
+public class BinaryTree {
+
+    public Node addNode(int data ,Node head) {
+        Node tempHead = head;
+        Node n = Node.newNode(data);
+        if(head == null) {
+            head = n;
+            return  head;
+        }
+
+        Node prev = null;
+
+        while(head != null) {
+            prev = head;
+            if(data > head.data) {
+                head = head.right;
+            } else {
+                head = head.left;
+            }
+        }
+
+        if(data> prev.data) {
+            prev.right = n;
+        } else {
+            prev.left = n;
+        }
+
+        return tempHead;
+    }
+}
